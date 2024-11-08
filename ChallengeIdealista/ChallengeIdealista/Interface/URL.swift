3c91526e6af69b408caf880b832cb6a44fffa1a2
franmu94 <fmalourarte@gmail.com>
2024-11-09 00:59:20 +0100

    cambios

--- ChallengeIdealista/ChallengeIdealista/Interface/URL.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-
-extension URL {
-    static let propertiesListURL = URL(string:"https://idealista.github.io/ios-challenge/list.json")!
-    static let propertiesDeatilURL = URL(string:"https://idealista.github.io/ios-challenge/detail.json")!
-}
